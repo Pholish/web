fa327ef372b127c24586afa0156328536054d2e8
Pholish <rogue.pholish@gmail.com>
2017-07-07 22:46:02 +0300

    complete

--- time is money/js/js.js
+++ time is money/js/js.js
@@ -1,13 +1,37 @@
-var options = {
-  // Set options
-        var options = {
-            offset: '#showHere',
-            offsetSide: 'top',
-            classes: {
-                clone:   'banner--clone',
-                stick:   'banner--stick',
-                unstick: 'banner--unstick'
-            }
-        };
-}
-var header = new Headhesive('.banner', options);
+$(function() {
+					$('a[href*="#"]:not([href="#"])').click(function() {
+						if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+							var target = $(this.hash);
+							target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+							if (target.length) {
+								$('html, body').stop().animate({
+									scrollTop: target.offset().top
+								}, 600);
+							return false;
+							}
+						}
+					});
+				});
+jQuery(document).ready(function(){
+    	    jQuery('.slider').slick({
+						dots:false,
+    	     	infinite: true,
+    	     	fade: true,
+    	     	cssEase: 'linear',
+    	    });
+    	
+
+				$('.hover-button').hover(function() {
+					$(this).parents('.service').find('.hover-js').addClass('hover');
+				},function() {
+					$(this).parents('.service').find('.hover-js').removeClass('hover');
+				});
+			
+
+				$(window).scroll(function(){
+  				var sticky = $('.headhesive'),
+							scroll = $(window).scrollTop();
+					if (scroll >= 100) sticky.addClass('fixed');
+					else sticky.removeClass('fixed');
+				
+			});
